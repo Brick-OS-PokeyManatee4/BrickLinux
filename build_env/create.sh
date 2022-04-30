8f871f3eb2de8fd354af0eff833603c958d7c551
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-04-30 12:45:30 -0500

    Add libstandardc++

--- build_env/create.sh
+++ build_env/create.sh
@@ -3,8 +3,9 @@ echo "LFS Location: ${LFS:?}"
 
 
 echo Building Toolchain and distro commands
-bash build_scripts/binutils-1.sh
-bash build_scripts/gcc-1.sh
-bash build_scripts/linux-headers.sh
-bash build_scripts/glibc.sh
+bash -e build_scripts/binutils-1.sh
+bash -e build_scripts/gcc-1.sh
+bash -e build_scripts/linux-headers.sh
+bash -e build_scripts/glibc.sh
+bash -e build_scripts/libstdcpp.sh
 echo Done
