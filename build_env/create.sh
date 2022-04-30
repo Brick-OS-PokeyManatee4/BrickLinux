e625993fd324d557a8afe659dffbc99b1edcbef2
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-04-30 03:30:59 -0500

    Update

--- build_env/create.sh
+++ build_env/create.sh
@@ -5,4 +5,6 @@ echo "LFS Location: ${LFS:?}"
 echo Building Toolchain and distro commands
 bash build_scripts/binutils-1.sh
 bash build_scripts/gcc-1.sh
+bash build_scripts/linux-headers.sh
+bash build_scripts/glibc.sh
 echo Done
