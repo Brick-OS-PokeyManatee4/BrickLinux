51d561078f59741d340d9724dc0879e99ce50e32
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-04-30 01:08:39 -0400

    Update create.sh

--- build_env/create.sh
+++ build_env/create.sh
@@ -1,10 +1,8 @@
 echo "BrickLinux Distro Root: ${DIST_ROOT:?}"
 echo "LFS Location: ${LFS:?}"
 
-if ! test $(whoami) == "distrobuild" ; then
-    echo "Must run as distrobuild!"
-    exit -1
-fi
 
 echo Building Toolchain and distro commands
 bash build_scripts/binutils-1.sh
+bash build_scripts/gcc-1.sh
+echo Done
